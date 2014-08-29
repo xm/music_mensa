f17eb0d20d6d90c77a449a8239029221ffd0db58
Christian Moniz <xianmoniz@gmail.com>
2014-08-29 00:15:14 -0700

    ugly lyrics

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -1,2 +1,9 @@
 module TracksHelper
+  def ugly_lyrics(lyrics)
+    return "no lyrics to display" if lyrics.nil?
+
+    lyrics.split("\r\n").map do |l| 
+      l.length > 0 ? "♫ ".concat(l) : ""
+    end.join("\r\n")
+  end
 end
